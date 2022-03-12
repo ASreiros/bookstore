06ceb305e34f71fc3d054c00973b4fab23942422
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-03-12 15:15:35 +0200

    favorite done but not checked

--- app.js
+++ app.js
@@ -32,15 +32,18 @@ fetch('https://in3.dev/knygos/types/')
     const cont = document.querySelector(".container")
     let conText = ""
     
-    
     console.log(data1);
     console.log(data2);
+
+
     
     
     data1.forEach((a)=> {
     
     const typas = data2[a.type-1].title    
-    conText = conText + `<div class="knyga">
+    conText = conText + `<div id="book${a.id}" class="knyga">
+    <img class="emptystar" id="es${a.id}" src="./img/emptystar.png" alt="not favorited">
+    <img class="star noshow" id="s${a.id}" src="./img/star.png" alt="favorite">
     <img id="ibook${a.id}" class="ibook" src=${a.img} alt="knyga">
     <h4 id="nbook${a.id}" class="nbook">${a.title}</h4>
      <p id="abook${a.id}" class="abook">Autorius: ${a.author}</p>
@@ -48,32 +51,63 @@ fetch('https://in3.dev/knygos/types/')
      <p id="tbook${a.id}" class="tbook">${typas}</p>
     </div>`     
      });
+     cont.innerHTML = conText;
+
+
+
+     const bookArr = document.querySelectorAll(".knyga")
+     //Below checks if there is favorite books and assign status favorite to them. Not checked
+    let memoryArr = JSON.parse(localStorage.getItem("Lentynafavorite"))
+    if(!(memoryArr.length > 0)){
+        memoryArr = [];
+    }
+
+    memoryArr.forEach(y => {
+        const id = y;
+        document.querySelector(`#${id}`).classList.add("favoritebook")   
+        document.querySelector(`#es${id.substr(4)}`).classList.toggle("noshow")
+        document.querySelector(`#s${id.substr(4)}`).classList.toggle("noshow")
+
+    })
     
-    cont.innerHTML = conText;
+   //
+    //below generates click event
+    bookArr.forEach( b => {
+        b.addEventListener("click", () => {
+        const id = b.id  
+        console.log(id, `es${id.substr(4)}`); 
+        document.querySelector(`#${id}`).classList.add("favoritebook") //not checked also 
+        document.querySelector(`#es${id.substr(4)}`).classList.toggle("noshow")
+        document.querySelector(`#s${id.substr(4)}`).classList.toggle("noshow")
+        //block below checks if book favorited, and adds or removes it from memoryArr
+        memoryArr = JSON.parse(localStorage.getItem("Lentynafavorite"))
+        if (document.querySelector(`#es${id.substr(4)}`).classList.contains("noshow")) {
+            memoryArr.push(id);
+            localStorage.setItem("Lentynafavorite", JSON.stringify(memoryArr)) 
+        } else{
+            const tempArr = [];
+            memoryArr.forEach(z => {
+                if (z.id !== id) {
+              tempArr.push(z.id)      
+                }
+            })
+            localStorage.setItem("Lentynafavorite", JSON.stringify(tempArr))  
+        }
+        //
+
+
+        })
+    })
+
     }
 )   
 
  
 
 
+ 
 
-// function extractor1(){
-//     const a = 
-//     if (Array.isArray(a)){
-//         localStorage.removeItem("Lentyna1");
-//         return a
-//     } 
-// setTimeout(extractor1, 300)
-// }    
-
-// function extractor2(){
-//     const a = JSON.parse(localStorage.getItem("Lentyna2"))
-//     if (Array.isArray(a)){
-//         localStorage.removeItem("Lentyna2");
-//         return a
-//     } 
-// setTimeout(extractor1, 300)
-// }   
-
-
+// memoryArr = JSON.parse(localStorage.getItem("Lentynafavorite"))
+//         memoryArr.push(id);
+//         localStorage.setItem("Lentynafavorite", JSON.stringify(memoryArr))
 
